aaee9cc492fe0038fd234e3c3108606040278874
Pissens <apissens@kpmg.com>
2025-05-04 17:24:41 +0200

    Initial structure

---
+++ Hello.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May  4 16:26:41 2025
+
+@author: apissens
+"""
+
+def hello():
+    """Print "Hello World" and return None."""
+    print("Hello World")
+    
+hello()
